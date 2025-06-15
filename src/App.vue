b739ce98e063209b069fd89f4ac2a53a91b1c551
ideapadkn <k.karimov@tuit.uz>
2025-06-15 18:53:32 +0500

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
 <template>
-  <div class="min-h-screen bg-gray-100 w-full">
-    <main class="container mx-auto p-6">
+  <div class="main">
+    <main class="container">
       <router-view />
     </main>
   </div>
